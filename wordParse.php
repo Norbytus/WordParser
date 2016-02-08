51893e7b80e885f6c6f8ef48dc4f14b73aa232be
User Alex <Alex@Alex.subnet>
2016-02-08 12:36:13 +0300

    First commit

---
+++ wordParse.php
@@ -0,0 +1,66 @@
+<?php
+
+	class DocxToHtml
+	{
+		public $filePath 		= "";
+		public $html 			= "";
+		public $imgDataArray 	= [];
+		public $copyFilePath 	= "";
+		public $fileExtension 	= ['jpeg','png','jpg'];
+
+		public function __construct($file,$copyPath)
+		{
+			$data = [];
+			exec('/usr/local/bin/pandoc -s '.$file.' -t html5',$data);
+			$this->filePath 	= $file;
+			$this->html 		= implode($data);
+			$this->copyFilePath = $copyPath;
+			
+			/*if(!is_dir($copyPath))
+				mkdir($copyPath);*/
+		}
+
+		public function getHtml()
+		{
+			$doc = new DOMDocument();
+			$doc->loadHTML($this->html);
+			$body 	= $doc->getElementsByTagName('body');
+			$img 	= $doc->getElementsByTagName('img');
+			
+			if($img->length != 0){
+				$this->changeFileDestanation();
+				foreach($img as $image){
+					$srcImage = $image->attributes->getNamedItem('src')->value;
+					if(array_key_exists($srcImage,$this->imgDataArray))
+						$image->attributes->getNamedItem('src')->value = $this->imgDataArray[$srcImage]['new'];
+				}
+			}
+
+			return $doc->saveHTML();
+		}
+
+		private function changeFileDestanation()
+		{
+			$zip = new ZipArchive();
+			$zip->open($this->filePath);
+
+			for($i = 0;$i<$zip->numFiles;$i++){
+				$fileZipName = $zip->getNameIndex($i);
+				$fileZipInfo = pathinfo($fileZipName);
+				if(in_array($fileZipInfo['extension'],$this->fileExtension)){
+					$path = explode('/',$fileZipInfo['dirname']);
+					$this->imgDataArray[$path[1].'/'.$fileZipInfo['basename']] = ['currentDir'=>getcwd(),'fileName'=>$fileZipInfo['basename']];
+					$fileNewName = uniqid('DocZip',true).".".$fileZipInfo['extension'];
+					$this->imgDataArray[$path[1].'/'.$fileZipInfo['basename']] = ['new'=>$this->copyFilePath.'/'.$fileNewName];
+					copy("zip://".$this->filePath."#".$fileZipName,$_SERVER['DOCUMENT_ROOT'].$this->copyFilePath.'/'.$fileNewName);
+					//chmod($this->copyFilePath.'/'.$fileNewName,777);
+				}
+			}
+
+			$zip->close();
+		}
+	}
+	/*$docToHtml = new DocxToHtml(__DIR__.'/test.docx','test');
+	$test = $docToHtml->getHtml();
+	print_r($test);*/
+?>
